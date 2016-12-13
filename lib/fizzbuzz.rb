127c024eed36d503ef9de3bf0b5c16febf3660b3
PeteGH <peter@grant-hay.eu>
2016-12-13 15:29:16 +0100

    refactoring code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,12 @@
 def fizzbuzz(n)
-  if n % 3 == 0 && n % 5 == 0
-    'FizzBuzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else n
-  end
+  n % 15 == 0 ? "FizzBuzz" : n % 5 == 0 ? "Buzz" : n % 3 == 0 ? "Fizz" : n
 end
+  # if n % 3 == 0 && n % 5 == 0
+  #   'FizzBuzz'
+  # elsif n % 3 == 0
+  #   'Fizz'
+  # elsif n % 5 == 0
+  #   'Buzz'
+  # else n
+  # end
+# end 
